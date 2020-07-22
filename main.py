d75e5a2a5233f36b592300724b68fd91db95e6c8
daliborstakic <stakicdalibor64@gmail.com>
2020-07-22 09:54:55 +0200

    Added the render() function with a 20x20 grid.

--- main.py
+++ main.py
@@ -3,8 +3,55 @@ import pygame
 pygame.init()
 
 
+class Snake:
+    def __init__(self):
+        pass
+
+
+class Cube:
+    def __init__(self):
+        pass
+
+
+# Screen
+win = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("Snake")
+
+# Colors
+WHITE = (255, 255, 255)
+GRAY = (226, 226, 226)
+
+
+def draw_grid(w, rows):
+    gap = w // rows
+
+    # Initial x and y
+    x = 0
+    y = 0
+
+    for i in range(rows):
+        x = x + gap
+        y = y + gap
+
+        pygame.draw.line(win, GRAY, (x, 0), (x, w))
+        pygame.draw.line(win, GRAY, (0, y), (w, y))
+
+
+def render():
+    win.fill(WHITE)
+    draw_grid(500, 20)
+    pygame.display.update()
+
+
 def main():
-    pass
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        render()
 
 
 if __name__ == '__main__':
