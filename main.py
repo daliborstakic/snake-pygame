30080ca39f1eac40048e26be3b46a0c0257a43dc
daliborstakic <stakicdalibor64@gmail.com>
2020-07-22 10:11:06 +0200

    Added parameters inside __init__() function inside the snake class.

--- main.py
+++ main.py
@@ -3,13 +3,45 @@ import pygame
 pygame.init()
 
 
-class Snake:
-    def __init__(self):
+class Cube:
+    def __init__(self, start_pos, dir_x=1, dir_y=0, color=(0, 255, 0)):
+        pass
+
+    def move(self):
+        pass
+
+    def draw(self, win):
         pass
 
 
-class Cube:
-    def __init__(self):
+class Snake:
+    body = []  # List containing multiple cube objects which represent the body
+
+    # This is an dictionary which stores the point of turn so the snake won't turn as a whole,
+    # rather cube by cube
+
+    turns = {}
+
+    def __init__(self, color, pos):
+        self.color = color
+        self.pos = pos
+        self.head = Cube(pos)  # The head will be the first cube object
+        self.body.append(self.head)  # Adding the head to the body list
+
+        # Initial directions
+        self.dir_x = 1
+        self.dir_y = 0
+
+    def move(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def add_cube(self):
+        pass
+
+    def draw(self):
         pass
 
 
@@ -20,6 +52,10 @@ pygame.display.set_caption("Snake")
 # Colors
 WHITE = (255, 255, 255)
 GRAY = (226, 226, 226)
+
+# Clock
+clock = pygame.time.Clock()
+FPS = 10
 
 
 def draw_grid(w, rows):
@@ -47,6 +83,9 @@ def main():
     run = True
 
     while run:
+        clock.tick(FPS)
+        pygame.time.delay(50)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
